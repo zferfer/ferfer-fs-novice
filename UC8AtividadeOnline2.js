5c641aefe644713cb1a6148e95ffe9913b793748
zferfer <zfroxz@gmail.com>
2022-02-26 12:29:08 -0300

    realizado ajuste na atividade2 da UC8

--- UC8AtividadeOnline2.js
+++ UC8AtividadeOnline2.js
@@ -1,7 +1,9 @@
 var dataEvento = new Date('February 28, 2022 23:15:00')
 var diaAtual = new Date()
 const idadepermitida = 18
-let idade = 19
+
+let nomeParticipante = "Fernando"
+let idade = 20
 let participantes = ["Pedro","Tiago","João","Barquinho","Vasco","Gremio"]
 
 
@@ -10,16 +12,11 @@ console.log("Insira a data do evento:", dataEvento.toString())
 
 if (dataEvento.getTime() < diaAtual.getTime()) {
     console.log("Data inválida")
-} else {
-    console.log("")
-}
-
-console.log("Insira sua idade:",idade)
-
-if (idade < idadepermitida) {
+} else if (idade < idadepermitida) {
     console.log("Evento não permitido para menores de 18 anos")
-}   else if (participantes.length < 100) {
+} else if (participantes.length < 100) {
+    participantes.push(nomeParticipante)
     console.log("Cadastro efetuado com sucesso!")
-}   else {
+}  else {
     console.log("Capacidade de participantes excedido")
 }
